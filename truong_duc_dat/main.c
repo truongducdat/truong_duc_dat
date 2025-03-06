04d9c07290257b704912fb60cf0a8a18a3ecf314
truongducdat <okbaybe009@gmail.com>
2025-03-06 19:54:35 +0700

    bai1: nhap 2 so nguyen tim so lon nhat

--- truong_duc_dat/main.c
+++ truong_duc_dat/main.c
@@ -1,6 +1,19 @@
-#include<stdio.h>
+﻿#include <stdio.h>
 
-void main()
-{
-	printf("Hello World\n");
+int main() {
+    int a, b;  
+
+    
+    printf("Nhap hai so nguyen a va b: ");
+    scanf_s("%d %d", &a, &b);
+
+  
+    if (a > b) {
+        printf("Số lớn nhất là: %d\n", a);
+    }
+    else {
+        printf("Số lớn nhất là: %d\n", b);
+    }
+
+    return 0;
 }
