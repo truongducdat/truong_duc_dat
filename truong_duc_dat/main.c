6f289a930c431b7fa56b037dd5c5678d00ff250f
truongducdat <okbaybe009@gmail.com>
2025-03-08 22:01:11 +0700

    bai 2 tim max 3 so nguyen a b c

--- truong_duc_dat/main.c
+++ truong_duc_dat/main.c
@@ -1,49 +1,19 @@
 ﻿#include <stdio.h>
 
-#define PASSWORD 1234  
-#define LAN_THU_TOI_DA 3    
-#define SO_TIEN_TOI_DA 10000 
-
 int main() {
-    int mknd, lanthu = 0, sotien;
+    int a, b, c, max;
+    printf("nhap so nguyen a,b,c lan luot: ");
+    scanf_s("%d %d %d", &a, &b, &c);
 
-    while (lanthu < LAN_THU_TOI_DA) {
-        printf("Vui long nhap mat khau: ");
-        if (scanf_s("%d", &mknd) != 1) {  
-            printf("Nhap sai dinh dang nhap lai\n");
-            return 1;
-        }
-
-        switch (mknd == PASSWORD) {
-        case 1: 
-            while (1) {
-                printf("Vui long nhap so tien ban muon rut: ");
-                if (scanf_s("%d", &sotien) != 1) { 
-                    printf("Ko dung dinh dang\n");
-                    return 1;
-                }
-                if (sotien > SO_TIEN_TOI_DA) {
-                    printf("So tien vuot qua gioi han. Vui long nhap lai\n");
-                }
-                else {
-                    printf("Ban vua moi rut: %d VND\n", sotien);
-                    return 0;
-                }
-            }
-            break;
-
-        case 0: 
-            lanthu++;
-            if (lanthu < LAN_THU_TOI_DA) {
-                printf("Ban nhap sai mat khau lan thu %d\n", lanthu);
-                printf("Ban con %d lan nhap mat khau\n", LAN_THU_TOI_DA - lanthu);
-            }
-            else {
-                printf("Ban da nhap sai qua %d lan. Thoat chuong trinh!\n", LAN_THU_TOI_DA);
-            }
-            break;
-        }
+    max = a;
+    if (b > max)
+    {
+        max = b;
     }
+    if (c > max) {
+        max = c;
+    }
+    printf("Số lớn nhất trong ba số là: %d\n", max);
 
     return 0;
 }
