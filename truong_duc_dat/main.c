d01f1c1f719fa8f61862c446fcf7ca1d4a0be8cf
truongducdat <okbaybe009@gmail.com>
2025-03-13 19:12:22 +0700

    buổi 4: bài 7: Viết chương trình tính tích phân của f(x) = x^2. từ a đến b theo phương pháp hình than với tần số lấy mẫu 1000 lần

--- truong_duc_dat/main.c
+++ truong_duc_dat/main.c
@@ -1,22 +1,29 @@
 ﻿#include <stdio.h>
+#include <math.h>
+
+double f(double x) {
+    return pow(x, 2); 
+}
 
 int main() {
-    int n;
-    printf("Nhap so tu nhien: ");
-    scanf_s("%d", &n);
+    int a, b, n = 1000; 
+    double h, tong, x, tich_phan;
 
-    printf("Cac so nguyen to nho hon %d la:\n", n);
-    for (int i = 2; i < n; i++) {
-        int kt = 1;
-        for (int j = 2; j < i; j++) {
-            if (i % j == 0) {
-                kt = 0;
-                break;
-            }
-        }
-        if (kt) {
-            printf("%d ", i);
-        }
+    printf("Nhap a: ");
+    scanf_s("%d", &a);
+    printf("Nhap b: ");
+    scanf_s("%d", &b);
+
+    h = (double)(b - a) / n;
+    tong = f(a) + f(b);
+
+    for (int i = 1; i < n; i++) {
+        x = a + i * h;
+        tong += 2 * f(x);
     }
+
+    tich_phan = h * tong / 2;
+    printf("Tich phan cua f(x) = x^2 tren khoang [%d, %d] la: %.6lf\n", a, b, tich_phan);
+
     return 0;
 }
