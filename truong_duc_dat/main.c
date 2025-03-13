5645de56ad5542126156403e58ec0a064041e9b8
truongducdat <okbaybe009@gmail.com>
2025-03-13 18:36:27 +0700

    buoi 4,bai1.1: ổng của n số nguyên đầu tiên(chỉ cộng số chẵn)

--- truong_duc_dat/main.c
+++ truong_duc_dat/main.c
@@ -6,9 +6,11 @@ int main() {
     scanf_s("%d", &n);
 
     for (i = 1; i <= n; i++) {
-        tong += i;
+        if (i % 2 == 0) { 
+            tong += i;
+        }
     }
-    printf("Tong cua %d so tu nhien dau tien la: %d\n", n, tong);
+    printf("Tong cac so chan cua %d so tu nhien dau tien la: %d\n", n, tong);
 
     return 0;
 }
