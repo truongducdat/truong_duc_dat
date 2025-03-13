e38b4a93cd73b9ea72003f4132e7dc1e9c6f0292
truongducdat <okbaybe009@gmail.com>
2025-03-13 18:42:28 +0700

    buoi 4,bai2: Tính giai thừa của một số tự nhiên

--- truong_duc_dat/main.c
+++ truong_duc_dat/main.c
@@ -1,16 +1,21 @@
 ﻿#include <stdio.h>
-
+#include<math.h>
 int main() {
-    int n, i, tong = 0;
-    printf("nhap so tu nhien n: ");
+    int n, i;
+    long long giai_thua = 1;
+
+    printf("Nhap so tu nhien n: ");
     scanf_s("%d", &n);
 
-    for (i = 1; i <= n; i++) {
-        if (i % 2 == 0) { 
-            tong += i;
-        }
+    if (n < 0) {
+        printf("Khong the tinh giai thua cua so am.\n");
     }
-    printf("Tong cac so chan cua %d so tu nhien dau tien la: %d\n", n, tong);
+    else {
+        for (i = 1; i <= n; i++) {
+            giai_thua *= i;
+        }
+        printf("Giai thua cua %d la: %lld\n", n, giai_thua);
+    }
 
     return 0;
 }
