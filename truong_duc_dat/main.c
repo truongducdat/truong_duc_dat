363820a7d61674254e7a2aac0601523244f08205
truongducdat <okbaybe009@gmail.com>
2025-03-13 18:56:51 +0700

    buổi 4, bài 4: Tính tiền gửi ngân hàng sau n năm

--- truong_duc_dat/main.c
+++ truong_duc_dat/main.c
@@ -1,15 +1,22 @@
 ﻿#include <stdio.h>
 
 int main() {
-    int i, j;
+    double tien_gui, lai, tien_nhan;
+    int so_nam, i;
 
-    for (j = 2; j <= 9; j++) { 
-        printf("Bang cuu chuong %d:\n", j);
-        for (i = 1; i <= 9; i++) {
-            printf("%d x %d = %d\n", j, i, j * i);
-        }
-        printf("\n"); 
+    printf("Nhap so tien gui ban dau: ");
+    scanf("%lf", &tien_gui);
+    printf("Nhap lai suat moi nam (%%): ");
+    scanf("%lf", &lai);
+    printf("Nhap so nam gui: ");
+    scanf("%d", &so_nam);
+
+    tien_nhan = tien_gui;
+    for (i = 1; i <= so_nam; i++) {
+        tien_nhan += tien_nhan * lai / 100;
     }
+
+    printf("Sau %d nam, so tien nhan duoc la: %.2lf\n", so_nam, tien_nhan);
 
     return 0;
 }
