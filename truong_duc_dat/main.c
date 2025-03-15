567e4e293c06b536ce6ed2474f3c605a574468bd
truongducdat <okbaybe009@gmail.com>
2025-03-15 22:11:41 +0700

    buổi 5, Chuỗi(string): Bài 2: Xây dựng hàm. dùng để điếm có bao nhiêu ký tự space (dấu cách) trong chuỗi

--- truong_duc_dat/main.c
+++ truong_duc_dat/main.c
@@ -4,15 +4,17 @@
 int dem_so_ky_tu(const char chuoi[]) {
 	int dem = 0;
 	for (int i = 0; chuoi[i] != '\0'; i++) {
-		dem++;
+		if (chuoi[i] == ' ') {
+			dem++;
+		}
 	}
 	return dem;
 
 }
 int main() {
-	char str_1[] = "hello world!";
+	char str_1[] = "Hello I am Rataturi";
 	int so_ky_tu = dem_so_ky_tu(str_1);
-	printf("So ky tu trong chuoi la: %d\n", so_ky_tu);
+	printf("So ky tu space trong chuoi la: %d\n", so_ky_tu);
 	return 0;
 	
 }
