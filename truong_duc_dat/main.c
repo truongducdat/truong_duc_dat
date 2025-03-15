c0e45455656823ff2cefd7d651e5186e7dae170b
truongducdat <okbaybe009@gmail.com>
2025-03-15 22:53:57 +0700

    buổi 5, Chuỗi(string): Bài 4: Xây dựng hàm. dùng để điếm số từ (word) trong chuỗi

--- truong_duc_dat/main.c
+++ truong_duc_dat/main.c
@@ -1,21 +1,21 @@
 ﻿#include <stdio.h>
-#include <string.h>
+#include <ctype.h>
 
-void chuyen_chu_hoa(char str_1[])
-{
-	for (int i = 0; str_1[i] != '\0'; i++)
-	{
-		if (str_1[i] >= 'a' && str_1[i] <= 'z')
-		{
-			str_1[i] -= 32;
-		}
-	}
+int dem_tu(const char chuoi[]) {
+    int dem = 0;
+
+    for (int i = 0; chuoi[i] != '\0'; i++) {
+        if ((i == 0 || chuoi[i - 1] == ' ') && chuoi[i] != ' ') {
+            dem++;
+        }
+    }
+    return dem;
 }
-int main() {
-	char str_2[] = "hello world";
-	printf("chuoi ky tu truoc khi chuyen: %s\n", str_2);
-	chuyen_chu_hoa(str_2);
-	printf("chuoi ky tu sau khi chuyen: %s\n", str_2);
 
-	return 0;
+int main() {
+    char str1[] = "Hello World";
+    int so_tu = dem_tu(str1);
+
+    printf("So tu trong chuoi la: %d\n", so_tu);
+    return 0;
 }
