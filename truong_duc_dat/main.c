3cbc1b5a7c253662093bc29b293d3c9f2e8cdc3f
truongducdat <okbaybe009@gmail.com>
2025-03-18 17:19:25 +0700

    buổi 5, Mảng(Array): Bài 1: Xây dựng hàm. tính tổng các giá trị trong mảng arr

--- truong_duc_dat/main.c
+++ truong_duc_dat/main.c
@@ -1,27 +1,33 @@
 ﻿#include <stdio.h>
 
-void tim_vi_tri_c(const char str[]) {
-    int found = 0; 
-    printf("Cac vi tri cua ky tu 'c' trong chuoi: ");
-
-    for (int i = 0; str[i] != '\0'; i++) {
-        if (str[i] == 'c') { 
-            printf("%d ", i); 
-            found = 1; 
-        }
+int tinhTongMang(int arr[], int n) {
+    int tong = 0;
+    for (int i = 0; i < n; i++) {
+        tong += arr[i];
     }
-
-    if (!found) {
-        printf("Khong tim thay ky tu 'c' trong chuoi.");
-    }
-    printf("\n");
+    return tong;
 }
 
 int main() {
-    char str[] = "chao mung cac ban den voi buoi hoc 5";
-    printf("Chuoi ban dau: '%s'\n", str);
+    int n;
 
-    tim_vi_tri_c(str);
+    printf("Nhap so luong phan tu cua mang (toi da 100): ");
+    scanf_s("%d", &n);
+
+    if (n > 100 || n <= 0) {
+        printf("So luong phan tu khong hop le.\n");
+        return 1;
+    }
+
+    int arr[100]; 
+    printf("Nhap cac phan tu cua mang:\n");
+    for (int i = 0; i < n; i++) {
+        printf("Phan tu [%d]: ", i);
+        scanf_s("%d", &arr[i]);
+    }
+
+    int tong = tinhTongMang(arr, n);
+    printf("Tong cac gia tri trong mang la: %d\n", tong);
 
     return 0;
 }
