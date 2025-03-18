7b622f1d2bcf5835ee5cce65d2e175d018584ac4
truongducdat <okbaybe009@gmail.com>
2025-03-18 17:49:37 +0700

    buổi 5, Mảng(Array): Bai 4: Thay the cac so am trong mang bang 0

--- truong_duc_dat/main.c
+++ truong_duc_dat/main.c
@@ -1,40 +1,30 @@
 ﻿#include <stdio.h>
 
-void inViTriSoLonNhat(int arr[], int n) {
-    int max = arr[0];
-
-  
-    for (int i = 1; i < n; i++) {
-        if (arr[i] > max) {
-            max = arr[i];
-        }
-    }
-
- 
-    printf("Cac vi tri cua so lon nhat (%d) trong mang la: ", max);
+void thayTheSoAmBang0(int arr[], int n) {
     for (int i = 0; i < n; i++) {
-        if (arr[i] == max) {
-            printf("%d ", i);
+        if (arr[i] < 0) {
+            arr[i] = 0;
         }
     }
-    printf("\n");
 }
 int main() {
     int n;
-    printf("Nhap so luong phan tu cua mang (toi da 100): ");
+    printf("Nhap so luong phan tu cua mang: ");
     scanf_s("%d", &n);
 
-    if (n <= 0 || n > 100) {
-        printf("So luong phan tu khong hop le.\n");
-        return 1;
-    }
-
-    int arr[100];
+    int arr[100]; 
     printf("Nhap cac phan tu cua mang:\n");
     for (int i = 0; i < n; i++) {
         scanf_s("%d", &arr[i]);
     }
 
-    inViTriSoLonNhat(arr, n);
+    thayTheSoAmBang0(arr, n);
+
+    printf("Mang sau khi thay the so am bang 0:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
     return 0;
 }
