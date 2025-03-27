337e77252a7c93efff89b39a819ea3e74924d706
truongducdat <okbaybe009@gmail.com>
2025-03-27 13:50:51 +0700

    BTVN Struct: HocSinh

--- truong_duc_dat/main.c
+++ truong_duc_dat/main.c
@@ -1,104 +1,108 @@
 ﻿#include <stdio.h>
-//xây dựng kiểu dữ liệu mới để mô tả đổi tượng là phân số
+#include <string.h>
+
 typedef struct {
-    int tu;
-    int mau;
-} phanso;
-//viết hàm nhân 2 phân số
-phanso Nhan_phan_so(phanso ps_x,phanso ps_y) {
-    phanso kq;
-    kq.tu = ps_x.tu * ps_y.tu;
-    kq.mau = ps_x.mau * ps_y.mau;
-    return kq;
-}
-//viêt hàm chia 2 phân số
-phanso chia_phan_so(phanso ps_x, phanso ps_y) {
-    phanso kq;
-    kq.tu = ps_x.tu * ps_y.mau;
-    kq.mau = ps_x.mau * ps_y.mau;
-    return kq;
-}
-//viết hàm cộng 2 phân số
-phanso cong_phan_so(phanso ps_x, phanso ps_y) {
-    phanso kq;
+    char ten[50];
+    int tuoi;
+    float diemToan;
+    float diemVan;
+    float diemTrungBinh;
+    int xepLoai;
+} HocSinh;
 
-    if (ps_x.mau == ps_y.mau) {
-        kq.tu = ps_x.tu + ps_y.tu;
-        kq.mau = ps_x.mau;
+void tinh_diem_TB_xep_loai(HocSinh* mang_hs, int so_luong) {
+    for (int i = 0; i < so_luong; i++) {
+        mang_hs[i].diemTrungBinh = (mang_hs[i].diemToan + mang_hs[i].diemVan) / 2;
+        if (mang_hs[i].diemTrungBinh >= 8.0) {
+            mang_hs[i].xepLoai = 0;
+        }
+        else if (mang_hs[i].diemTrungBinh >= 6.5) {
+            mang_hs[i].xepLoai = 1;
+        }
+        else if (mang_hs[i].diemTrungBinh >= 5.0) {
+            mang_hs[i].xepLoai = 2;
+        }
+        else {
+            mang_hs[i].xepLoai = 3;
+        }
     }
-    else
-    {
-        kq.tu = ps_x.tu * ps_y.mau + ps_y.tu * ps_x.mau;
-        kq.mau = ps_x.mau * ps_y.mau;
-    }
-    return kq;
-}
-//viết hàm trừ 2 phần số
-phanso tru_phan_so(phanso ps_x, phanso ps_y) {
-    phanso kq;
-    if (ps_x.mau == ps_y.mau) {
-        kq.tu = ps_x.tu - ps_y.tu;
-        kq.mau = ps_x.mau;
-    }
-    else {
-        kq.tu = ps_x.tu * ps_y.mau - ps_y.tu * ps_x.mau;
-        kq.mau = ps_x.mau * ps_y.mau;
-    }
-    return kq;
-}
-//viết hàm rút gọn 2 phân số
-
-int ucln(int a, int b) {// Hàm tìm ước chung lớn nhất
-    while (b != 0) {
-        int r = a % b;
-        a = b;
-        b = r;
-    }
-    return a;
 }
 
-phanso rut_gon_phan_so(phanso ps) {// Hàm rút gọn phân số
-    int uc = ucln(ps.tu, ps.mau);
-    ps.tu /= uc;
-    ps.mau /= uc;
-
-    // Đưa dấu âm về tử số
-    if (ps.mau < 0) {
-        ps.tu *= -1;
-        ps.mau *= -1;
+void nhap_danh_sach(HocSinh* mang_hs, int n) {
+    for (int i = 0; i < n; i++) {
+        printf("Nhap thong tin hoc sinh thu %d:\n", i + 1);
+        printf("Ten: ");
+        fgets(mang_hs[i].ten, sizeof(mang_hs[i].ten), stdin);
+        mang_hs[i].ten[strcspn(mang_hs[i].ten, "\n")] = '\0';
+        printf("Tuoi: ");
+        scanf_s("%d", &mang_hs[i].tuoi);
+        printf("Diem Toan: ");
+        scanf_s("%f", &mang_hs[i].diemToan);
+        printf("Diem Van: ");
+        scanf_s("%f", &mang_hs[i].diemVan);
+        while ((getchar()) != '\n' && getchar() != EOF);
     }
-
-    return ps;
-}
-void in_phan_so(phanso ps) {
-    printf("%d/%d\n", ps.tu, ps.mau);
 }
 
-void main() {
-    phanso a = { 4, 6 };  
-    phanso b = { 2, 3 };  
+void in_danh_sach(HocSinh* mang_hs, int n) {
+    printf("\n%-20s %-5s %-10s %-10s %-10s %-10s\n", "Ten", "Tuoi", "Toan", "Van", "DiemTB", "XepLoai");
+    for (int i = 0; i < n; i++) {
+        char* xepLoaiStr;
+        switch (mang_hs[i].xepLoai) {
+        case 0: xepLoaiStr = "Gioi"; break;
+        case 1: xepLoaiStr = "Kha"; break;
+        case 2: xepLoaiStr = "TB"; break;
+        case 3: xepLoaiStr = "Yeu"; break;
+        default: xepLoaiStr = "Khong xac dinh"; break;
+        }
+        printf("%-20s %-5d %-10.2f %-10.2f %-10.2f %-10s\n",
+            mang_hs[i].ten, mang_hs[i].tuoi, mang_hs[i].diemToan,
+            mang_hs[i].diemVan, mang_hs[i].diemTrungBinh, xepLoaiStr);
+    }
+}
 
-    printf("Phan so A = ");
-    in_phan_so(a);
+HocSinh tim_dtb_hs_cao_nhat(HocSinh* mang_hs, int soluong) {
+    HocSinh max = mang_hs[0];
+    for (int i = 1; i < soluong; i++) {
+        if (mang_hs[i].diemTrungBinh > max.diemTrungBinh) {
+            max = mang_hs[i];
+        }
+    }
+    return max;
+}
 
-    printf("Phan so B = ");
-    in_phan_so(b);
+void sap_xep_theo_diemTB(HocSinh* mang_hs, int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (mang_hs[i].diemTrungBinh < mang_hs[j].diemTrungBinh) {
+                HocSinh temp = mang_hs[i];
+                mang_hs[i] = mang_hs[j];
+                mang_hs[j] = temp;
+            }
+        }
+    }
+}
 
-    phanso tong = rut_gon_phan_so(cong_phan_so(a, b));
-    printf("A + B = ");
-    in_phan_so(tong);
+int main() {
+    int n;
+    printf("Nhap so luong hoc sinh: ");
+    scanf_s("%d", &n);
+    while ((getchar()) != '\n' && getchar() != EOF);
 
-    phanso hieu = rut_gon_phan_so(tru_phan_so(a, b));
-    printf("A - B = ");
-    in_phan_so(hieu);
+    HocSinh mang_hs[100];
+    nhap_danh_sach(mang_hs, n);
+    tinh_diem_TB_xep_loai(mang_hs, n);
 
-    phanso tich = rut_gon_phan_so(Nhan_phan_so(a, b));
-    printf("A * B = ");
-    in_phan_so(tich);
+    printf("\nDanh sach hoc sinh:\n");
+    in_danh_sach(mang_hs, n);
 
-    phanso thuong = rut_gon_phan_so(chia_phan_so(a, b));
-    printf("A / B = ");
-    in_phan_so(thuong);
+    HocSinh hs_max = tim_dtb_hs_cao_nhat(mang_hs, n);
+    printf("\nHoc sinh co diem trung binh cao nhat:\n");
+    printf("%s - Tuoi: %d - Diem TB: %.2f\n", hs_max.ten, hs_max.tuoi, hs_max.diemTrungBinh);
+
+    sap_xep_theo_diemTB(mang_hs, n);
+    printf("\nDanh sach hoc sinh sau khi sap xep theo diem trung binh giam dan:\n");
+    in_danh_sach(mang_hs, n);
 
     return 0;
 }
